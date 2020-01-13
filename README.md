8d08383b28636621f541d7b11ecbbd8dcaabcc04
James Kennedy <jameskennedy@jamess-mbp.home>
2020-01-13 10:08:01 +0000

    Whileloop 100 pages of postings, extract to CSV

---
+++ README.md
@@ -0,0 +1,11 @@
+This project is to scrape job postings in Toronto and analyse them to get an 
+understanding of what's available.
+
+#####Plan
+1. Identify target sites to scrape.  
+2. Build a robust scraper.  
+3. Build a pipeline to store scraped data in a database.
+4. Analyse the data for insite into the jobs market in Toronto.
+5. Blog outlining the experience.
+
+
--- indeed_scraper.py
+++ indeed_scraper.py
@@ -1,34 +1,35 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
+import pandas as pd
 
-#TODO - Investigate picked object to ensure scraping done correctly
-#TODO - define range for all available posting
-#TODO - need to protect spider from detection
-#TODO - if hyperlink returned for company look at scraping that page
 
 def ReturnJobs():
-    base_url='https://ca.indeed.com/jobs?q=&l=Toronto%2C+ON&rbl=Toronto%2C+ON&jlid=aaab304de0fc22ae&jt=fulltime&lang=en&sort=date'
-    url_list = [base_url + f'&start{i}' for i in range(10,100,10)]
-    url_list.insert(0,base_url)
+    url='https://ca.indeed.com/jobs?q=&l=Toronto%2C+ON&rbl=Toronto%2C+ON&jlid=aaab304de0fc22ae&jt=fulltime&lang=en&sort=date'
     browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
+    browser.implicitly_wait(10)
     all_jobs = []
-    for url in url_list:
+    x = 1
+    while x < 101:
+        if x % 10 == 0: time.sleep(20)
         browser.get(url)
         html_doc = browser.page_source
         soup = BeautifulSoup(html_doc, 'html.parser')
         jobs = soup.find_all('div', class_='jobsearch-SerpJobCard unifiedRow row result clickcard')
         all_jobs += jobs
+        partial_url = soup.find('div', class_='pagination').find('b').next_sibling.next_sibling['href']
+        url = 'https://ca.indeed.com/' + partial_url
+        x += 1
     return(all_jobs)
 
-#TODO - add rest of url to partial link
 
 def ParseJobs(jobs):
     postings = []
     for job in jobs:
         posting=dict()
         posting['job_title'] = job.find(class_='title').find('a').text.strip()
-        posting['job_link'] = job.find(class_='title').find('a')['href']
+        job_partial_url = job.find(class_='title').find('a')['href']
+        posting['job_link'] = 'https://ca.indeed.com/' + job_partial_url
         try:
             posting['hiring_company'] = job.find(class_='company').find('a').text.strip()
         except AttributeError:
@@ -36,9 +37,11 @@ def ParseJobs(jobs):
         posting['location'] = job.find(class_='location').text.strip()
         posting['brief_sumary'] = job.find(class_='summary').text.strip()
         postings.append(posting)
-    return(postings)
+    df_jobs = pd.DataFrame(postings)
+    todays_date = time.strftime('%Y %m %d')
+    df_jobs.to_csv(todays_date+'.csv')
+    return
 
-#TODO - Function that returns additional data from a specific job url 
 
 if __name__ == '__main__':
     pass
