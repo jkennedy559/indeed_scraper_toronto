7331ca03409a7f15c05e82ff2481625e5e0b9d28
James Kennedy <jameskennedy@jamess-mbp.home>
2020-02-05 10:43:24 +0000

    Script corrupted, recreated from remote copy

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
 
 
 def ReturnJobs():
     url='https://ca.indeed.com/jobs?q=&l=Toronto%2C+ON&rbl=Toronto%2C+ON&jlid=aaab304de0fc22ae&jt=fulltime&lang=en&sort=date'
-    browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    browser = webdriver.Chrome(options=chrome_options)
     browser.implicitly_wait(10)
     all_jobs = []
     x = 1
@@ -17,7 +20,10 @@ def ReturnJobs():
         soup = BeautifulSoup(html_doc, 'html.parser')
         jobs = soup.find_all('div', class_='jobsearch-SerpJobCard unifiedRow row result clickcard')
         all_jobs += jobs
-        partial_url = soup.find('div', class_='pagination').find('b').next_sibling.next_sibling['href']
+        try:
+            partial_url = soup.find('div', class_='pagination').find('b').next_sibling.next_sibling['href']
+        except:
+            return(all_jobs)
         url = 'https://ca.indeed.com/' + partial_url
         x += 1
     return(all_jobs)
@@ -39,12 +45,11 @@ def ParseJobs(jobs):
         postings.append(posting)
     df_jobs = pd.DataFrame(postings)
     todays_date = time.strftime('%Y %m %d')
-    df_jobs.to_csv(todays_date+'.csv')
+    output_destination = '~/PycharmProjects/indeed_scraper_toronto/'
+    df_jobs.to_csv(output_destination+todays_date+'.csv')
     return
 
 
 if __name__ == '__main__':
-    pass
-
-
-
+    jobs = ReturnJobs()
+    ParseJobs(jobs)
